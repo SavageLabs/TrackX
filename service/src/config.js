c798d6de6d8efd435034f3bdb5c7ee88efcde1c5
Liz3 <accs@21xayah.com>
2019-08-26 04:45:23 +0200

    fixed a plugin

--- service/src/config.js
+++ service/src/config.js
@@ -1,6 +1,14 @@
 module.exports = {
-    botToken: "",
-    serverId: "",
-    channelId: "615310342914048002",
-    allowed: [ "savageftop", "savagefactions", "savageskyblock", "obsidianbreaker", "savagefactionx", "savagecorex", "savageoutpost"]
-}
+  botToken: "",
+  serverId: "",
+  channelId: "615310342914048002",
+  allowed: [
+    "savageftop",
+    "savagefactions",
+    "savageskyblock",
+    "obsidianbreaker",
+    "savagefactionsx",
+    "savagecorex",
+    "savageoutpost"
+  ]
+};
