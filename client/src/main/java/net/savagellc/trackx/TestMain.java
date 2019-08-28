8bd0bf40faf88be489a0b7189eceed7ca86a80da
Liz3 <accs@21xayah.com>
2019-08-28 16:10:16 +0200

    changed debugging approach

--- client/src/main/java/net/savagellc/trackx/TestMain.java
+++ client/src/main/java/net/savagellc/trackx/TestMain.java
@@ -2,7 +2,7 @@ package net.savagellc.trackx;
 
 public class TestMain {
     public static void main(String[] args) {
-        TrackX.startTracking("test-plugin", "1.0", "savagellc");
+     //   TrackX.startTracking("test-plugin", "1.0", "savagellc");
         new Thread(() -> {
             try {
                 Thread.sleep(5000);
--- client/src/main/java/net/savagellc/trackx/TrackX.java
+++ client/src/main/java/net/savagellc/trackx/TrackX.java
@@ -6,6 +6,9 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Vector;
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
+import org.bukkit.Bukkit;
 
 public class TrackX {
 
@@ -15,6 +18,7 @@ public class TrackX {
     private String version;
     private String includes;
     private static boolean reporting = false;
+    private static boolean failed = false;
 
     private TrackX(String pluginId, String version, String inludes) {
         this.pluginId = pluginId;
@@ -22,57 +26,88 @@ public class TrackX {
         this.includes = inludes;
     }
 
+    static void handle(String asString) {
+        for (String key : created.keySet()) {
+            if (asString.contains(created.get(key).includes)) {
+                new Thread(() -> {
+                    created.get(key).handleException(asString);
+                }).start();
+                break;
+            }
+        }
+    }
+
     static {
-        Thread.setDefaultUncaughtExceptionHandler((t, e) -> {
-            new Thread(() -> {
-                String asString = Utils.throwableToString(e);
-                for (String key : created.keySet()) {
-                    if (asString.contains(created.get(key).includes)) {
-                        created.get(key).handleException(asString);
-                        break;
+        try {
+            Handler handler = new Handler() {
+
+                @Override
+                public void publish(LogRecord record) {
+                    if (record.getThrown() != null) {
+                        String handleError = record.getMessage() + "\n" + Utils.throwableToString(record.getThrown());
+                        handle(handleError);
                     }
                 }
-            }).start();
-            e.printStackTrace();
-        });
+
+                @Override
+                public void flush() {
+                }
+
+                @Override
+                public void close() throws SecurityException {
+                }
+            };
+            Bukkit.getLogger().addHandler(handler);
+        } catch (Exception e) {
+            System.out.println("[TrackX] Failed to attach handler");
+            failed = false;
+        }
     }
 
     private void pushUpdate(String preparedMessage) throws IOException {
-        HttpURLConnection connection = (HttpURLConnection) new URL("https://savage-error-tracker.appspot.com/report").openConnection();
+        HttpURLConnection connection = (HttpURLConnection) new URL("https://savage-error-tracker.appspot.com/report")
+                .openConnection();
         connection.setRequestProperty("Content-type", "application/x-www-form-urlencoded");
         connection.setRequestMethod("POST");
         connection.setDoInput(true);
         connection.setDoOutput(true);
         connection.getOutputStream().write(preparedMessage.getBytes());
         connection.getOutputStream().flush();
-        connection.getResponseCode();
+        int responseCode = connection.getResponseCode();
+        System.out.println("[TrackX] Response code: " + responseCode);
+        byte[] available = new byte[connection.getInputStream().available()];
+        connection.getInputStream().read(available);
+        System.out.println("[TrackX] Response Message: " + new String(available));
         connection.disconnect();
     }
 
     private void handleException(String s) {
-        if(reporting) return;
+        if (reporting)
+            return;
         reporting = true;
         if (s.contains(includes)) {
             if (!reportedTraces.contains(s)) {
+                System.out.println("[TrackX] Reporting issue...");
                 reportedTraces.add(s);
-                if (reportedTraces.size() > 30) reportedTraces.remove(0);
+                if (reportedTraces.size() > 30)
+                    reportedTraces.remove(0);
                 StringBuilder builder = new StringBuilder();
                 try {
                     builder.append(Utils.encodeParam("pluginId", pluginId));
                 } catch (UnsupportedEncodingException e) {
-                    //should never happen
+                    // should never happen
                 }
                 builder.append("&");
                 try {
                     builder.append(Utils.encodeParam("version", version));
                 } catch (UnsupportedEncodingException e) {
-                    //should never happen
+                    // should never happen
                 }
                 builder.append("&");
                 try {
                     builder.append(Utils.encodeParam("trace", s));
                 } catch (UnsupportedEncodingException e) {
-                    //should never happen
+                    // should never happen
                 }
                 try {
                     pushUpdate(builder.toString());
@@ -85,7 +120,11 @@ public class TrackX {
     }
 
     public static void startTracking(String pluginId, String version, String includes) {
-        if (created.containsKey(pluginId)) return;
+        if (failed)
+            return;
+        if (created.containsKey(pluginId))
+            created.remove(pluginId);
+        System.out.println("[TrackX] Tracker starting for " + pluginId);
         TrackX instance = new TrackX(pluginId, version, includes);
         created.put(pluginId, instance);
     }
