dc27ac48fe8bf920bb86e84ba37c020d5596437c
Liz3 <accs@21xayah.com>
2019-10-14 21:38:10 +0200

    Update paste.js

--- service/src/paste.js
+++ service/src/paste.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 
 module.exports = (content) => {
     return new Promise((resolve, reject) => {
-        axios.post("https://paste.savagellc.net/api/add", { language: "plaintext", content}, {headers: { "Content-type": "application/json"}}).then(res => {
+        axios.post("https://paste.illyria.io/api/add", { language: "plaintext", content}, {headers: { "Content-type": "application/json"}}).then(res => {
             const { data } = res;
             if(data.success) {
                 resolve(data.id);
